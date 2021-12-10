0894a1681e88ea57c876f897aaa3a43ef26d11ba
unknown <melkyrafa@outlook.com>
2021-12-09 21:17:24 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -237,7 +237,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.lenght !== 1){
+  if (letra.lenght >= 1){
     return "Dato incorrecto"
   } 
   if (letra==="a"||letra==="e"||letra==="i"||letra==="o"||letra==="u"){
